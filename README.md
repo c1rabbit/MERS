ffef61124cc0e7fe414406dedc0675bc1ebd3964
c1rabbit <mail@calvinthanh.com>
2017-01-28 20:40:06 -0800

    basic algo done

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # MERS
+
+* install node module `npm install mers --save`
+* use in application `var mers = require('mers')`
+
+## Usage
+`mers.generate(orgID, loanNum)`
+* input
+ * orgID: your 6 digit MERs org ID
+ * loanNum: loan number not to exceed 9 digits
+* output
+ * one checksum digit
+
+`mers.validate(minNum)`
+* input
+ * minNum: 16 digit Mortgage Instrument Number(MIN)
+* output
+ * boolean: checks if MIN is a valid format
--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-
-console.log("test");
-
 //mod 10 weight 2
 function generate (orgID,loanNum) {
   if (orgID.toString().length != 6 ){
     return "[error]: org ID is not 6 digits";
   } else if (loanNum.toString().length > 9){
     return "[error]: loan number is longer than 9 digits"
-  } else{
-    var temp = orgID.toString() + loanNum.toString();
-    var sum = 0;
-    for(var i = 0; i < temp.length; i++){
-      if(i % 2 == 0){
-        var x2 = parseInt(temp[i])*2;
-        if(x2 > 9){
-          sum += 1 + x2 % 10;
-          //console.log(1 + x2 % 10);
-        }else{
-          sum += x2;
-          //console.log(x2);
-        }
-      }else{
-        sum += parseInt(temp[i]);
-        //console.log(temp[i]);
-      }
+  } else if (loanNum.toString().length < 9){
+    while(loanNum.toString().length < 9){
+      loanNum = "0" + loanNum;
     }
-    //console.log("sum" + sum);
-    var digit = Math.ceil(sum/10)*10 - sum
-    //console.log(Math.ceil(sum/10)*10);
-    return digit;
+    console.log("orgID: " + orgID + "\tloanNum: " + loanNum);
+  } else{
+    console.log("orgID: " + orgID + "\tloanNum: " + loanNum);
   }
+  var temp = orgID.toString() + loanNum.toString();
+  var sum = 0;
+  for(var i = 0; i < temp.length; i++){
+    if(i % 2 == 0){
+      var x2 = parseInt(temp[i])*2;
+      if(x2 > 9){
+        sum += 1 + x2 % 10;
+        //console.log(1 + x2 % 10);
+      }else{
+        sum += x2;
+        //console.log(x2);
+      }
+    }else{
+      sum += parseInt(temp[i]);
+      //console.log(temp[i]);
+    }
+  }
+  //console.log("sum" + sum);
+  var digit = Math.ceil(sum/10)*10 - sum
+  //console.log(Math.ceil(sum/10)*10);
+  return digit;
+
 }
 
 function validate (min){
@@ -37,13 +41,11 @@ function validate (min){
   return digit == min.toString().substring(15);
 }
 
-// exports.printMsg = function() {
-//   console.log("This is a message from the demo package");
-// }
-
 console.log(generate("0123","0123"));
 console.log(generate("123456","0123456789"));
-console.log(generate("123456","01234"));
+console.log("--------------");
+console.log(generate("123456","012034"));
+console.log(generate("125647","1"));
 console.log(generate("111111","111111111"));
 console.log(validate("1111111111111117"));
 console.log(generate("516311","111111111"));
